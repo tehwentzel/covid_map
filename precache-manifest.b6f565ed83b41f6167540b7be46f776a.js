bc3e4414f6d07c5d043460e8c75af74d3c59b9da
Andrew Wentzel <tehwentzel@gmail.com>
2023-09-12 15:09:35 -0500

    Updates

---
+++ precache-manifest.b6f565ed83b41f6167540b7be46f776a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc27cd814a4d89dc170cbddcc57e5890",
+    "url": "/covid_map/index.html"
+  },
+  {
+    "revision": "94290042177c22f25788",
+    "url": "/covid_map/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "135f8ce47c8ec7d58962",
+    "url": "/covid_map/static/css/main.3368a75c.chunk.css"
+  },
+  {
+    "revision": "94290042177c22f25788",
+    "url": "/covid_map/static/js/2.54a70f6b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid_map/static/js/2.54a70f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "135f8ce47c8ec7d58962",
+    "url": "/covid_map/static/js/main.85cde6cd.chunk.js"
+  },
+  {
+    "revision": "351ce7a75fa9bf16e1bd",
+    "url": "/covid_map/static/js/runtime-main.69cc49d5.js"
+  }
+]);
